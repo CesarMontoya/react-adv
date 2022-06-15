ea004bfda89e9c32da40443a9e33c22ed9df21d6
Cesar Montoya <cesardmontoya@outlook.com>
2022-06-14 22:21:26 -0500

    Control Props Alt

--- src/02-components-patterns/hooks/useShopingCart.ts
+++ src/02-components-patterns/hooks/useShopingCart.ts
@@ -6,22 +6,15 @@ export const useShoppingCart = () => {
 
 	const onProductCountChange = ({ count, product }: { count: number; product: Product }) => {
 		setShoppingCart((oldShoppingCart) => {
-			const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
-
-			// Si el contador es mayor a cero, el producto existe en el cart.
-			if (Math.max(productInCart.count + count, 0) > 0) {
-				productInCart.count += count;
-
-				return {
-					...oldShoppingCart,
-					[product.id]: productInCart,
-				};
+			if (count === 0) {
+				const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+				return rest;
 			}
 
-			// Cuando el contador es cero o menor, significa que no exite el producto en el cart,
-			// por lo tanto, se debe borrar del objeto de productos
-			const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-			return rest;
+			return {
+				...oldShoppingCart,
+				[product.id]: { ...product, count },
+			};
 		});
 	};
 
